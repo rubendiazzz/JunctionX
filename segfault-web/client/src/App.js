cf23dd1ba3d4bcf2b11253aa844e9059108f59dc
Rubén Díaz Fraga <rubendfraga@gmail.com>
2023-10-20 20:36:27 +0200

    Server modificado

--- segfault-web/client/src/App.js
+++ segfault-web/client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [image, setImage] = useState(null);
+
+  const login = async () => {
+    try {
+      const res = await axios.post('http://localhost:5000/login', { username, password });
+      setImage(res.data.image);
+    } catch (err) {
+      console.error(err);
+      alert("Login failed");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={login}>Login</button>
+      {image && <img src={image} alt="User" />}
     </div>
   );
 }
